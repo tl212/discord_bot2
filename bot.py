375535cf70dac5e75b05977d94a66b535efbd3f5
Tom Lee <tom.kennedy.lee@gmail.com>
2020-07-11 03:08:02 -0400

    bot file

--- bot.py
+++ bot.py
@@ -4,3 +4,30 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.env
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!test'):
+        await message.channel.send('This is a test')
+
+    if message.content == 'stream join':
+        channel = message.author.voice.channel
+        await channel.connect()
+
+client.run(TOKEN)
+
+if __name__ == '__main__':
+    client = StreamBot()
+    client.run(TOKEN)
+
+    
